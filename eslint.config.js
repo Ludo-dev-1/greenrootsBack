353e9183f659682692c02bc19ec14b1cfce3f3a3
Ludo-dev-1 <thibaud.ludovic@sfr.fr>
2025-02-06 13:53:52 +0100

    config eslint ok

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+export default {
+  languageOptions: {
+    globals: {
+      process: "readonly", // Exemple pour Node.js
+    },
+    parserOptions: {
+      ecmaVersion: 2021, // Utilise ECMAScript 2021
+      sourceType: "module", // Si tu utilises des modules ES
+    },
+  },
+
+  rules: {
+    semi: ["error", "always"],
+    eqeqeq: ["error", "always"],
+    "no-unused-vars": ["warn"],
+    quotes: ["error", "double"],
+    camelcase: ["error", { properties: "always" }],
+    "no-trailing-spaces": "error",
+    indent: ["error", 2],
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+    "no-alert": "error",
+  },
+};
