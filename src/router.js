ac30834b4369ea06e2aa340529a4ed1385bde431
Nathan Begue <nathan.begue@oclock.school>
2025-02-11 16:54:07 +0100

    Validation des données avec Joi pour le CRUD terminée

--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ import { controllerWrapper as cw } from "./utils/controllerWrapper.js";
 import mainController from "./controllers/main.controller.js";
 import { authenticate } from "./middlewares/auth.middleware.js";
 import authController from "./controllers/auth.controller.js";
-import joiValidator from "./middlewares/joiValidator.middleware.js";
+import { registerJoiValidator, crudJoiValidator } from "./middlewares/joiValidator.middleware.js";
 import shopController from "./controllers/shop.controller.js";
 
 const router = Router();
@@ -18,8 +18,8 @@ router.get("/boutique/:id", cw(mainController.getOneArticle));
 // BOUTIQUE CRUD
 router.get("/api/articles", authenticate, cw(shopController.getAllArticles));
 router.get("/api/articles/:id", authenticate, cw(shopController.getOneArticle));
-router.post("/api/articles", authenticate, cw(shopController.createArticleWithPicture)); // Validation des données lors de la création d'un article : modifier pour utiliser une méthode dans joiValidator.middleware.js
-router.patch("/api/articles/:id", authenticate, cw(shopController.updateArticle));
+router.post("/api/articles", authenticate, crudJoiValidator, cw(shopController.createArticleWithPicture)); // Validation des données lors de la création d'un article : modifier pour utiliser une méthode dans joiValidator.middleware.js
+router.patch("/api/articles/:id", authenticate, crudJoiValidator, cw(shopController.updateArticle));
 router.delete("/api/articles/:id", authenticate, cw(shopController.deleteArticle));
 
 // COMMANDE
@@ -31,7 +31,7 @@ router.get("/compte", authenticate, cw(mainController.getUserProfile)); //user
 router.get("/compte/suivi", authenticate, cw(mainController.getOrderTracking)); //user et order et tracking
 // INSCRIPTION
 router.get("/inscription", cw(authController.registerUserForm));
-router.post("/inscription", joiValidator, cw(authController.register));
+router.post("/inscription", registerJoiValidator, cw(authController.register));
 // CONNEXION
 router.get("/connexion", cw(authController.loginUserForm));
 router.post("/connexion", cw(authController.login));
